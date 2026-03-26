789d5af7ed27be8b8fe40679c19dd25d1929dc00
leo-mj <leo.maedje@gmail.com>
2026-03-26 13:44:40 +0100

    Deployment: Set up for deployment on GitHub Pages of the deploy branch

--- app/vite.config.js
+++ app/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/assistive-equilibrium/" : "/",
   plugins: [react()],
   test: {
     environment: "node",
@@ -11,4 +12,4 @@ export default defineConfig({
       include: ["src/utils/**", "src/hooks/**"],
     },
   },
-});
+}));
